22e0deaa6e45f7a41f516318d3649b51205ce721
Kadir <kadirercen@gmail.com>
2023-10-26 17:00:01 +0300

    26.10

--- client/src/components/OpenStreetMap.js
+++ client/src/components/OpenStreetMap.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup, Circle, CircleMarker } from "react-leaflet";
-
+import { MapContainer, TileLayer, Popup, CircleMarker } from "react-leaflet";
+import MarkerClusterGroup from 'react-leaflet-cluster'
 
 
 function OpenStreetMap() {
 
+    const [logs, setLogs] = useState([]);
+
+    const getLogs = async () => {
+      try {
+          const response = await fetch("http://localhost:5000/logs")
+          const jsonData = await response.json();
+          
+          setLogs(jsonData);
+          console.log(logs)
+          if (jsonData.length > 0) {
+            // Access properties for the first object in the array
+            console.log(jsonData[0].longitude, jsonData[0].latitude, jsonData[0].id);
+          }
+      } catch (err) {
+          console.error(err.message);            
+      }
+  }
+    useEffect(() => {
+      getLogs();
+    }, [])
+
     const center = [52.22977, -0.19117];
 
     const points = [
+      
         {
           lat: 51.489096,
           lng: -0.19117,
@@ -28,14 +50,25 @@ function OpenStreetMap() {
           lng: -0.897892,
           title: 'point 4'
         },
+        {
+          lat: 52.217726,
+          lng: -2.239895,
+          title: 'point 5'
+        },
+        {
+          lat: 51.489096,
+          lng: -0.19117,
+          title: 'point 6'
+        }
       ];
       
-      const MyMarkers = ({ data }) => {
+      
+    const MyMarkers = ({ data }) => {
         return data.map(({ lat, lng, title }, index) => (
           <CircleMarker
             key={index}
             center={{ lat, lng }}
-            radius={7}
+            radius={2}
             color="red"
           >
             <Popup>{title}</Popup>
@@ -49,7 +82,9 @@ function OpenStreetMap() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <MyMarkers data={points} />
+            <MarkerClusterGroup chunkedLoading>
+              <MyMarkers data={points} />
+            </MarkerClusterGroup>
         </MapContainer>
   );
 }
